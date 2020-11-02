aea35f940abd1992d07630f90e1ca3612fc53fce
BaileyWatkins <BaileyWatkins@DESKTOP-AEL058Q>
2020-11-02 11:10:01 -0600

    Record by Record Upload

---
+++ OnlineEducation/client/Intput.js
@@ -0,0 +1,36 @@
+import { Template } from 'meteor/templating';
+
+
+Template.RecordByRecordTemplate.events({
+    'click #uploadRecord':function(event){
+        console.log("Inserting Data");
+        var case_id = document.getElementById('caseid_text').value;
+        var hospital_code = document.getElementById('hosptialcode_text').value;
+        var hospital_type_code = document.getElementById('hospitaltypecode_text').value;
+        var city_code_hospital = document.getElementById('citycode_text').value;
+        var hospital_region_code = document.getElementById('regioncode_text').value;
+        var available_rooms = document.getElementById('availablebeds_text').value;
+        var department = document.getElementById('departmnet_text').value;
+        var ward_type = document.getElementById('wardtype_text').value;
+        var ward_facility_code = document.getElementById('wardcode_text').value;
+        var bed_grade = document.getElementById('bedgrade_text').value;
+        var patientid = document.getElementById('patientid_text').value;
+        var city_code_patient = document.getElementById('citycodepatient_text').value;
+        var type_of_admission = document.getElementById('type_text').value;
+        var severity_of_illness = document.getElementById('severity_text').value;
+        var visitors_with_patient = document.getElementById('visitors_text').value;
+        var age = document.getElementById('age_text').value;
+        var admission_deposit = document.getElementById('admission_text').value;
+        var stay_date = document.getElementById('stay_text').value;
+        var functionname = "UploadRecordByRecord";
+        
+         Meteor.call(functionname.toString(),case_id, hospital_code, hospital_type_code,
+         city_code_hospital, hospital_region_code, available_rooms, department, ward_type, ward_facility_code,
+         bed_grade, patientid, city_code_patient, type_of_admission, severity_of_illness, visitors_with_patient,
+         age, admission_deposit, stay_date, function(err, res){
+               if (err) console.log("mongoDB Error");
+               else console.log("Record Submission Success");
+         })
+         alert("1 document inserted");;
+      },
+});
--- OnlineEducation/server/main.js
+++ OnlineEducation/server/main.js
@@ -137,5 +137,81 @@ Meteor.startup(() => {
         return future.wait();
       },
 
+      UploadRecordByRecord:function(case_id, hospital_code, hospital_type_code,
+        city_code_hospital, hospital_region_code, available_rooms, department, ward_type, ward_facility_code,
+        bed_grade, patientid, city_code_patient, type_of_admission, severity_of_illness, visitors_with_patient,
+        age, admission_deposit, stay_date){ //Inserts each field into a database
+        console.log("Inside Query");
+
+
+          //Datbase fields
+        var fieldname1 = "case_id";
+        var fieldname2 = "Hospital_code";        
+        var fieldname3 = "Hospital_type_code";
+        var fieldname4 = "City_Code_Hospital";
+        var fieldname5 = "Hospital_region_code";
+        var fieldname6 = "Available Extra Rooms in Hospital";
+        var fieldname7 = "Department";
+        var fieldname8 = "Ward_Type";
+        var fieldname9 = "Ward_Facility_Code";
+        var fieldname10 = "Bed Grade";
+        var fieldname11 = "patientid";
+        var fieldname12 = "City_Code_Patient";
+        var fieldname13 = "Type of Admission";
+        var fieldname14 = "Severity of Illness";
+        var fieldname15 = "Visitors with Patient";
+        var fieldname16 = "Age";
+        var fieldname17 = "Admission_Deposit";
+        var fieldname18 = "Stay";
+
+
+       
+
+
+        var finalQuery = {}; //creates query params
+
+        //Adds each item to the query
+        finalQuery[fieldname1] = case_id;
+        finalQuery[fieldname2] = hospital_code;
+        finalQuery[fieldname3] = hospital_type_code;
+        finalQuery[fieldname4] = city_code_hospital;
+        finalQuery[fieldname5] = hospital_region_code;
+        finalQuery[fieldname6] = available_rooms;
+        finalQuery[fieldname7] = department;
+        finalQuery[fieldname8] = ward_type;
+        finalQuery[fieldname9] = ward_facility_code;
+        finalQuery[fieldname10] = bed_grade;
+        finalQuery[fieldname11] = patientid;
+        finalQuery[fieldname12] = city_code_patient;
+        finalQuery[fieldname13] = type_of_admission;
+        finalQuery[fieldname14] = severity_of_illness;
+        finalQuery[fieldname15] = visitors_with_patient;
+        finalQuery[fieldname16] = age;
+        finalQuery[fieldname17] = admission_deposit;
+        finalQuery[fieldname18] = stay_date;
+        
+        console.log(finalQuery);
+
+          var http = require("http");
+          var MongoClient = require("mongodb").MongoClient;
+          var url="mongodb://localhost:27017/";
+          var future = new Future();
+          MongoClient.connect(url, function(err,db){ //connects to DB
+            if (err) throw err;
+            console.log("inside query");
+            console.log(finalQuery);
+            var dbo = db.db("Hospital"); //set db name
+            //inserts 1 record
+            dbo.collection("RawData").insertOne(finalQuery, function(err, res) {
+              if (err) throw err;
+              console.log("1 document inserted");
+              //future.return(res); This doesnt seem to work for some reason not sure why
+              db.close();
+             });
+          }
+          );
+          return future.wait();
+        }
+
   });
 });
