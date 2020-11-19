324baeee97d1cf2637882e60fc9883d119ebd838
BaileyWatkins <BaileyWatkins@DESKTOP-AEL058Q>
2020-11-19 17:45:28 -0600

    Fixed required feild and stop page refresh onsubit

--- OnlineEducation/client/DataView.js
+++ OnlineEducation/client/DataView.js
@@ -7,12 +7,6 @@ Template.DataView.events({
         alert("This will take a while, and might crash the browesr.")
         console.log("Beginning Query")
 
-        paginator({
-            table: document.getElementById("table_box_native").getElementsByTagName("table")[0],
-            box: document.getElementById("index_native"),
-            active_class: "color_page"
-        });
-
 
         Meteor.call('ViewData',function(err, res){
 
